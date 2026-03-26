dc96e04f4942c8385f9b0fb9f73cb2fa7f8c3fd6
anubhav-vandkar <av3336@columbia.edu>
2026-03-26 16:12:06 -0400

    circle clipping

--- lab3-sw/vga_ball.h
+++ lab3-sw/vga_ball.h
@@ -24,7 +24,7 @@ typedef struct {
 #define WIDTH 640
 #define HEIGHT 480
 
-#define VGA_BALL_RADIUS 16
+#define VGA_BALL_RADIUS 32
 
 /* ioctls and their arguments */
 #define VGA_BALL_WRITE_BACKGROUND _IOW(VGA_BALL_MAGIC, 1, vga_ball_arg_t)
